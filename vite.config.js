e6ec6a572afd37b0186445d9a623f6b831212b17
Rex Ciavola <rexiiichav@gmail.com>
2024-03-25 23:19:04 -0400

    maybe done with prod push

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/climate-changed/",
   plugins: [react()],
+  base: "/climate-changed/",
   test: {
     globals: true,
     environment: "jsdom",
